f2af2698f8c70412837fa607ca8b35c66cc88ffe
spsarras <psarras.st@gmail.com>
2023-11-09 13:28:57 +0000

    basic info

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
+=
+TestNaming.py,f\1\f1e574760edcaa10e40c9c749ba71cbb0d2a0692
---
+++ main.py
@@ -0,0 +1,28 @@
+﻿import argparse
+
+
+# Generate name gives you a name based on the value and parameter
+# e.x. for first structure with height 10 and second structure with height 20
+# the name should be H10-H20.gsa
+# if you need to provide a date then
+# the name should be 20231009_H10-H20.gsa
+
+# The name should optionally include the date
+# the name should include the heights of the two structures showing from and to
+# if the transition is on ground it should have the first height and then the second,
+# it should be the other way around if not.
+# if the height for the second structure is zero it should only display the height of the first structure
+# you will always have a height for the first structure
+
+def main(value: int, parameter: int) -> str:
+    name = f"{value}-{parameter}"
+    print(name)
+    return name
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Hello World!")
+    parser.add_argument('-v', '--value', type=int, help='one of the important values')
+    parser.add_argument('-p', '--parameter', type=int, help='one of the important parameters')
+    args = parser.parse_args()
+    main(args.value, args.parameter)
